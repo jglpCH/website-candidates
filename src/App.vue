0c6d922687aa51df77ff4bb8ac8338e68c63a272
Manuel Frick <manuel.frick@elinica.ch>
2018-11-30 22:10:36 +0100

    scss and fonts

--- src/App.vue
+++ src/App.vue
@@ -18,13 +18,10 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    @import "style/globals";
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
