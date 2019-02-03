678c6f9f960344dfee87024b717cdf1449880bee
Manuel Frick <manuel.frick@elinica.ch>
2019-02-03 20:36:39 +0100

    fix youtube styling

--- src/routes/about/About.vue
+++ src/routes/about/About.vue
@@ -64,4 +64,18 @@
   /deep/ .smartspider {
     max-width: 100%;
   }
+
+  /deep/ .videoWrapper {
+    position: relative;
+    padding-bottom: 66.6%; /* 16:9 */
+    padding-top: 25px;
+    height: 0;
+  }
+  /deep/ .videoWrapper iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
 </style>
