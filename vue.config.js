2b7642b044298002a6dedecd2e4e1f225143255f
Manuel Frick <manuel.frick@elinica.ch>
2019-02-07 17:46:51 +0100

    enable analytics tracking

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,23 @@
 const path = require('path');
 const PrerenderSpaPlugin = require('prerender-spa-plugin');
 
+const trackingCodes = {
+  beni: 'UA-134076868-2',
+  jeanine: 'UA-134076868-4',
+  lukas: 'UA-134076868-3',
+  manuel: 'UA-134076868-1',
+  vivi: 'UA-134076868-5',
+};
+
+const buildFor = 'manuel';
+
+process.env.VUE_APP_ANALYTICS_TAG = trackingCodes[buildFor];
+
 module.exports = {
   configureWebpack: {
     resolve: {
       alias: {
-        '@content': path.resolve(__dirname, 'src/content/manuel'),
+        '@content': path.resolve(__dirname, 'src/content/' + buildFor),
       }
     },
     module: {
